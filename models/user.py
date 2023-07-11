7f8a132849479816f82651b0cb9f0e7db7049129
ambkingdavid <kelvinwooldbridge@gmail.com>
2023-07-11 09:25:10 +0100

    add models, engine

--- models/user.py
+++ models/user.py
@@ -6,17 +6,26 @@ from models.base_model import BaseModel, Base
 from os import getenv
 from sqlalchemy import Column, String, UniqueConstraint
 from sqlalchemy.orm import relationship
+from hashlib import md5
 
 
 class User(BaseModel, Base):
     """Representation of a user """
     if models.storage_t == 'db':
         __tablename__ = 'users'
+        password = Column(String(128), nullable=False)
         username = Column(String(128), nullable=False, unique=True)
         __table_args__ = (UniqueConstraint('username', name='unique_username'),)
     else:
         username = ""
+        password = ""
 
     def __init__(self, *args, **kwargs):
         """initializes user"""
         super().__init__(*args, **kwargs)
+
+    def __setattr__(self, name, value):
+        """sets a password with md5 encryption"""
+        if name == "password":
+            value = md5(value.encode()).hexdigest()
+        super().__setattr__(name, value)
